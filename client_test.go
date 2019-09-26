cce1d270b7905cc8aac841a6ce48629699351445
Chao Guo <jeffguorg@gmail.com>
2019-09-26 11:12:07 +0800

    Error now implement error interface

--- client_test.go
+++ client_test.go
@@ -1,18 +1,19 @@
 package jsonrpc_test
 
 import (
+	"github.com/jeffguorg/jsonrpc"
 	"testing"
 )
 
 var (
-	endpoint = Endpoint("http://127.0.0.1:6800/jsonrpc")
+	endpoint = jsonrpc.Endpoint("http://127.0.0.1:6800/jsonrpc")
 )
 
 func TestEndpoint_Call(t *testing.T) {
-	if res, err := endpoint.Call(Request{
+	if res, err := endpoint.Call(jsonrpc.Request{
 		Method: "aria2.addUri",
 		ID:     1,
-		Parameters: [][]string{
+		Parameters: []interface{}{
 			[]string{"https://mirrors.tuna.tsinghua.edu.cn/archlinux/iso/latest/archlinux-2019.09.01-x86_64.iso"},
 		},
 	}); err != nil {
--- types.go
+++ types.go
@@ -1,15 +1,23 @@
 package jsonrpc
 
+import "fmt"
+
 type Error struct {
-	Code    string      `json:"code"`
+	Code    int         `json:"code"`
 	Message string      `json:"message"`
 	Data    interface{} `json:"data,omitempty"`
 }
 
+var _ error = Error{}
+
+func (err Error) Error() string {
+	return fmt.Sprintf("JsonRPC Error(%d): %s(%+v)", err.Code, err.Message, err.Data)
+}
+
 type Request struct {
-	Version    string      `json:"jsonrpc"`
-	Method     string      `json:"method"`
-	ID         interface{} `json:"id"`
+	Version    string        `json:"jsonrpc"`
+	Method     string        `json:"method"`
+	ID         interface{}   `json:"id"`
 	Parameters []interface{} `json:"params,omitempty"`
 }
 
